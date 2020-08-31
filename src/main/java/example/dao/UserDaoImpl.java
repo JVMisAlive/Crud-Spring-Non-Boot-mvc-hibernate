b4f779c849fb2a44ad30bdc1f86f2929737461c5
JVMisAlive <67837530+JVMisAlive@users.noreply.github.com>
2020-08-31 13:33:08 +0300

    Update UserDaoImpl.java

--- src/main/java/example/dao/UserDaoImpl.java
+++ src/main/java/example/dao/UserDaoImpl.java
@@ -11,9 +11,6 @@ import java.util.List;
 public class UserDaoImpl implements UserDao {
 
     @PersistenceContext
-    private EntityManager em;
-
-    @PersistenceContext
     private EntityManager entityManager;
 
     @SuppressWarnings("unchecked")
